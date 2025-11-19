07258565ea1dc566e20dc1a96f6981637bc28ad5
Connor Gao <88961746+Connor315@users.noreply.github.com>
2025-11-18 20:17:49 -0500

    Auth (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,6 @@
 'use client';
-
-import { Button } from '@/components/ui/button';
-// import { useSession } from 'next-auth/react';
-import { Cloud } from 'lucide-react';
-import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-    // const { data: session } = useSession();
-    // const session = {
-    //     user: {
-    //         name: 'Test User',
-    //         email: 'test@example.com'
-    //     }
-    // };
-
-    // return (
-    //     <div className="flex flex-col items-center justify-center">
-    //         <Cloud size={60} className="mb-3 text-blue-500" />
-    //         <h1 className="text-6xl font-bold mb-4">Welcome to NASlite</h1>
-    //         <p className="text-xl mb-8">Your lightweight NAS solution.</p>
-
-    //         {session && (
-    //             <div className="flex justify-center mt-5">
-    //                 <Button className="text-xl mt-5 p-8" size="lg" variant="outline">
-    //                     <Link href="/files">Browse My Files</Link>
-    //                 </Button>
-    //             </div>
-    //         )}
-    //     </div>
-    // );
     redirect('/signin');
 }
